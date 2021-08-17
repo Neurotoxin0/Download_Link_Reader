fd56f3fbeb60bde59f53416493ff6c8fb7e70f51
Don <Xusi101066@163.com>
2021-08-17 15:45:08 +0800

    Add Zip Support

--- Read_Files.py
+++ Read_Files.py
@@ -1,19 +1,27 @@
-import os
+import os, zipfile
 
 path = os.path.split(os.path.realpath(__file__))[0]     # get current path  (where script is located)
-os.chdir(path)                                          # changine working dir to current path
+os.chdir(path)                                          # changing working dir to current path
+
 
 '''Process Single File'''
-def process(file):
-    tmp = open(file, "r")
+def process(txt_file):
+    tmp = open(txt_file, "r")
     lines = tmp.readlines()
     print(lines)
     tmp.close()
 
-'''Main'''
-for item in os.listdir(path):
-    if item.endswith('txt'):
-        print("\n------------------------")
-        print("Processing File: " + item + "\n")
-        process(item)
-        print("\n------------------------")
+
+'''Process Zip File'''
+def unzip():
+    print("\n------------------------")
+    for file in os.listdir(path):
+        if file.endswith('zip'):
+            print("Unziping File: " + file)
+            tmp = zipfile.ZipFile(file)
+            for files in tmp.namelist(): tmp.extract(files)
+            tmp.close()
+    print("------------------------")
+
+
+unzip()
