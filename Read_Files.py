eaf24d4f9f88fd765e008bbaf4b94700f0978e90
Don <Xusi101066@163.com>
2021-08-18 12:47:17 +0800

    V1
    Support plain txt, single zipped file

--- Read_Files.py
+++ Read_Files.py
@@ -1,27 +1,70 @@
-import os, zipfile
+import os, shutil, zipfile
 
-path = os.path.split(os.path.realpath(__file__))[0]     # get current path  (where script is located)
-os.chdir(path)                                          # changing working dir to current path
-
-
-'''Process Single File'''
-def process(txt_file):
-    tmp = open(txt_file, "r")
-    lines = tmp.readlines()
-    print(lines)
-    tmp.close()
+keyword = ["ed2k"]                                      # keywork for download link
+ignore_list = ["output.txt"]                            # ignore txt file with certain names
+path = os.path.split(os.path.realpath(__file__))[0]     
+os.chdir(path)                                          
 
 
 '''Process Zip File'''
 def unzip():
-    print("\n------------------------")
+    print("\n------------------------Unziping------------------------")
+    
+    archived_dir = './archived'
+    if not os.path.exists(archived_dir):
+        os.makedirs(archived_dir)
+
     for file in os.listdir(path):
-        if file.endswith('zip'):
+        if file.endswith('.zip'):
             print("Unziping File: " + file)
             tmp = zipfile.ZipFile(file)
-            for files in tmp.namelist(): tmp.extract(files)
+            
+            for files in tmp.namelist(): 
+                tmp.extract(files)
+            
             tmp.close()
-    print("------------------------")
+            shutil.move(file, archived_dir)
+    
+    print("------------------------Finish------------------------\n")
 
 
-unzip()
+'''Process Files'''
+def process():
+    print("------------------------Processing------------------------")
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            os.chdir(root)
+            
+            if file.endswith('.txt'):
+                for ignore in ignore_list:
+                    if file != ignore:
+                        print("------------------------")
+                        search(file)
+                        print("------------------------")
+
+    out.close()
+
+    print("------------------------Finish------------------------\n")
+
+
+'''Looking For Download Link'''        
+def search(txt_file):
+    print("Processing File: " + txt_file)
+    tmp = open(txt_file, "r")
+    
+    for line in tmp.readlines():
+        for key in keyword:
+            if key in line:
+                print(line + "\n")
+                out.writelines(line + "\n")
+                
+    tmp.close()
+   
+
+
+
+if __name__ == '__main__':
+    unzip()
+    out = open("output.txt", "a")
+    process()
