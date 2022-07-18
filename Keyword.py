b1d5016038106e4bf9193111518edabeaa62bae0
Don <Xusi101066@163.com>
2022-07-18 00:28:23 -0400

    Create Keyword.py

---
+++ Keyword.py
@@ -0,0 +1,27 @@
+from Common import Log, Read_Config
+
+def Main(filepath):
+    
+    Keyword, Ignore_content = None, None
+    links, count = [], 0
+
+    # Load cfg
+    Config = Read_Config()
+    try:
+        Keyword = Config['KeyWord']
+        Ignore_content = Config['Ignore Content']
+    except: return None, -1     # Bad Config
+        
+    # Read lines
+    file = open(filepath, "r", encoding='utf-8')
+    for line in file.readlines():
+        link = any(key if key in line else False for key in Keyword)
+        Ignore = any(key if key in line else False for key in Ignore_content)
+
+        if link and not Ignore:
+            line = line.strip("\n")
+            Log.logger.info(line + "\n")
+            links.append(line)
+            count += 1
+    
+    return links, count
