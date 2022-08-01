c85f6e7f24fd3357122b08837f0db059c56d07b4
Don <Xusi101066@163.com>
2022-08-01 16:09:30 -0500

    Update Common.py

--- Common.py
+++ Common.py
@@ -58,7 +58,7 @@ def Message(option, back, Language = -1):
         10: ["取消删除", "Deletion Canceled"][Language],
         11: ["配置已损坏！", "Bad Config File!"][Language],
         12: ["关键字已存在! 输入新行数来修改, 或留空来返回", "Keyword Exist! Input New # Of Lines To Update, Or Input <SPACE> To Go Back"][Language],
-        13: ["请输入行数", "Please Input # Of Lines"][Language],
+        13: ["请输入行数 --- 找到关键字后的第几行;例如：1代表关键字后一行，-1代表关键字前一行，0代表关键字本身", "Please Input # Of Lines --- # of line after keyword matched; e.g. 1 -> 1 line after keyword, -1 -> line before keyword, 0 -> keyword line"][Language],
     }
     Log.logger.info(message[option])
     if back: Log.logger.info(["按任意键来返回", "Press Any Key To Go Back", "按任意键来返回 | Press Any Key To Go Back"][Language]); Read()
